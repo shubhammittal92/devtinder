124e0d9384cbee1b7c0579f9ff3190c11fe233ad
Shubham mittal <shubmittal.92@gmail.com>
2025-02-06 20:24:36 +0530

    get-apis

--- src/app.js
+++ src/app.js
@@ -3,15 +3,16 @@ const connectDB = require("./config/database")
 const app = express();
 const User = require("./models/user")
 
+// app.use(() => {
+
+// })
+
+app.use(express.json());
+
 app.post('/signup', async (req, res) =>{
 
 
-const user = new User({
-    firstName: 'John',
-    lastName: 'Doe',
-    emailId : "sh@gmail.com",
-    password: "aks@123"
-});
+const user = new User(req.body);
 
 
 try{
@@ -25,6 +26,38 @@ catch(err){
 }
 
 });
+
+
+app.get('/user' , async(req, res) =>{
+    const userEmail = req.body.emailId;
+
+    try{
+        const users = await User.find({emailId: userEmail});
+        if(users.length == 0)
+        {
+            res.status(404).send("user not found");
+        }
+        else
+        {
+            res.send(users);
+        }
+    }
+    catch(err){
+        res.status(400).send("something went wrong");
+    }
+});
+
+
+
+app.get("/feed", async (req, res) =>{
+     try{
+           const users = await User.find({});
+           res.send(users);
+     }
+     catch(err){
+        res.status(400).send("something went wrong");
+    }
+})
 
 
 connectDB().then(() =>{
