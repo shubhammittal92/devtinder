824b71d2d2695bb2447eeccfe773c4d2184581f4
Shubham mittal <shubmittal.92@gmail.com>
2025-02-21 20:17:58 +0530

    chat feature

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const http = require("http");
 
 app.use(
     cors({
@@ -19,19 +20,24 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
+const initializeSocket = require("./utils/socket");
+const chatRouter = require("./routes/chat");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
+app.use("/", chatRouter);
 
 
 
+const server = http.createServer(app);
+initializeSocket(server);
 
 // Connect to Database and Start Server
 connectDB().then(() => {
     console.log("Database connected");
-    app.listen(3000, () => {
+    server.listen(3000, () => {
         console.log("Listening on port 3000");
     });
 }).catch(err => {
