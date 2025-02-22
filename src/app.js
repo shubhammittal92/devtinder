c891969abc9b1d16036e9122a414a2de667352b1
Shubham mittal <shubmittal.92@gmail.com>
2025-02-22 23:00:53 +0530

    port chnage

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const http = require("http");
 
 const app = express();
-const port = process.env.PORT || 4000; // Default to 4000 if PORT is not set
+const port = process.env.PORT || 3000; // Default to 4000 if PORT is not set
 
 app.use(
   cors({
